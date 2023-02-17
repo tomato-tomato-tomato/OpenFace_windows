8d853c53b0b5a32879d101b3ce308b3420c2cc8a
tomato-tomato-tomato <79496523+tomato-tomato-tomato@users.noreply.github.com>
2023-02-17 17:33:11 +1100

    Update FeatureExtraction_loop.R
    Should print success/failure messages after processing

--- FeatureExtraction_loop.R
+++ FeatureExtraction_loop.R
@@ -1,5 +1,10 @@
-#Code retrieved using get_commands as part of OpenFaceR package 
-#Change directory to where openface is installed 
-cd C:/Users/manvis/Desktop/openface
+#Change directory to where openface is installed, eg: cd C:/Users/manvis/Desktop/openface
 #creates a loop to process all videos located at C:/Users/manvis/Desktop/failed and saves to C:/Users/manvis/Desktop/failed_processed
-for /F %i in ('dir /b "C:/Users/manvis/Desktop/failed"') do FeatureExtraction.exe  -inroot "C:/Users/manvis/Desktop/failed" -f %i  -3Dfp -pose -aus -gaze -out_dir "C:/Users/manvis/Desktop/failed_processed"
+for /F %i in ('dir /b "C:/Users/manvis/Desktop/failed"') do (
+  FeatureExtraction.exe -inroot "C:/Users/manvis/Desktop/failed" -f %i -3Dfp -pose -aus -gaze -out_dir "C:/Users/manvis/Desktop/failed_processed"
+  if %errorlevel%==0 (
+    echo %i processed successfully.
+  ) else (
+    echo %i processing failed.
+  )
+)
