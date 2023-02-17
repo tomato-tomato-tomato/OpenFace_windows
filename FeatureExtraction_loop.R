9e124c1f2706618a82e0eaf9fa09d6726b825c97
tomato-tomato-tomato <79496523+tomato-tomato-tomato@users.noreply.github.com>
2023-02-17 18:28:48 +1100

    Update FeatureExtraction_loop.R
    #This updated command uses the >> operator to append the success or error message for each file to a separate text file. The success messages will be stored in a file called success.txt, while the error messages will be stored in a file called error.txt, both located in the C:/Users/manvis/Desktop/failed_processed directory.

--- FeatureExtraction_loop.R
+++ FeatureExtraction_loop.R
@@ -1,10 +1,13 @@
-#Change directory to where openface is installed, eg: cd C:/Users/manvis/Desktop/openface
-#creates a loop to process all videos located at C:/Users/manvis/Desktop/failed and saves to C:/Users/manvis/Desktop/failed_processed
 for /F %i in ('dir /b "C:/Users/manvis/Desktop/failed"') do (
   FeatureExtraction.exe -inroot "C:/Users/manvis/Desktop/failed" -f %i -3Dfp -pose -aus -gaze -out_dir "C:/Users/manvis/Desktop/failed_processed"
   if %errorlevel%==0 (
-    echo %i processed successfully.
+    echo %i processed successfully. >> "C:/Users/manvis/Desktop/failed_processed/success.txt"
   ) else (
-    echo %i processing failed.
+    echo %i processing failed. >> "C:/Users/manvis/Desktop/failed_processed/error.txt"
   )
 )
+
+#This updated command uses the >> operator to append the success or error message for each file to a separate text file. The success messages will be stored in a file called success.txt, while the error messages will be stored in a file called error.txt, both located in the C:/Users/manvis/Desktop/failed_processed directory.
+
+
+
