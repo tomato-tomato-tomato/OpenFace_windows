192ccdbc8d19956b5ddbab1942ae2e62fceb2a92
tomato-tomato-tomato <79496523+tomato-tomato-tomato@users.noreply.github.com>
2023-04-18 00:13:08 +1000

    Update FeatureExtraction_loop.R

--- FeatureExtraction_loop.R
+++ FeatureExtraction_loop.R
@@ -1,13 +1,13 @@
-for /F %i in ('dir /b "C:/Users/manvis/Desktop/failed"') do (
-  FeatureExtraction.exe -inroot "C:/Users/manvis/Desktop/failed" -f %i -3Dfp -pose -aus -gaze -out_dir "C:/Users/manvis/Desktop/failed_processed"
+for /F %i in ('dir /b "C:/Users/manvis/Desktop/batch1"') do (
+  FeatureExtraction.exe -inroot "C:/Users/manvis/Desktop/batch1" -f %i -3Dfp -pose -aus -gaze -out_dir "C:/Users/manvis/Desktop/failed_processed"
   if %errorlevel%==0 (
-    echo %i processed successfully. >> "C:/Users/manvis/Desktop/failed_processed/success.txt"
+    echo %i processed successfully. >> "C:/Users/manvis/Desktop/batch1_processed/success.txt"
   ) else (
-    echo %i processing failed. >> "C:/Users/manvis/Desktop/failed_processed/error.txt"
+    echo %i processing batch1. >> "C:/Users/manvis/Desktop/batch1_processed/error.txt"
   )
 )
 
-#This updated command uses the >> operator to append the success or error message for each file to a separate text file. The success messages will be stored in a file called success.txt, while the error messages will be stored in a file called error.txt, both located in the C:/Users/manvis/Desktop/failed_processed directory.
+#This updated command uses the >> operator to append the success or error message for each file to a separate text file. The success messages will be stored in a file called success.txt, while the error messages will be stored in a file called error.txt, both located in the C:/Users/manvis/Desktop/batch1_processed directory.
 
 
 
